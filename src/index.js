a32a2309538553fe396012748a8266518a5998f2
polowis <tranphuochung0912@gmail.com>
2020-07-26 15:39:24 +1000

    fix: improve game loop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 import Game from './engine/game';
 import TheLightOfMidnight from './game.js';
+import Intro from './state/Intro'
 
-const game = new TheLightOfMidnight(new Game());
-
+//const game = new TheLightOfMidnight(new Game());
+const game = new Intro(new Game());
 game.preload();
 
-setTimeout(() => {  game.render(); }, 2000);
+
+function loop(timeStamp) {
+    let progress = timeStamp - lastRender
+    game.update(progress)
+    lastRender = timeStamp
+    window.requestAnimationFrame(loop)
+
+}
+var lastRender = 0
+setTimeout(() => { 
+    game.render()
+    window.requestAnimationFrame(loop)}, 2000)
+
+//setTimeout(() => {  game.render(); while(true) { game.update()} }, 2000);
+
 
 
 
